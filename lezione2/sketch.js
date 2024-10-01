31847c89f27f1f8ebfcb9198f5482817ba3e65a6
Cristina2910 <cristina.pedrazzini@mail.polimi.it>
2024-10-01 13:03:05 +0200

    lezione2

---
+++ lezione2/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+//fare cielo scuro
+background("darkblue");
+//prato verde
+fill("darkgreen")
+rect(0, 200, 400, 300);
+//prima casetta
+fill("white")
+rect(30, 140, 60, 60)
+//tetto
+fill("red")
+triangle(160, 300, 240, 300, 200, 200)
+
+
+
+}
+
+
